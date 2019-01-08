68e53941f2ed53333ba887d5a7811447336ceace
Andy <tech-agent2@ipasspay.com>
2019-01-08 11:26:19 +0800

    -amaa

---
+++ example/config.php
@@ -0,0 +1,12 @@
+<?php
+$config = [
+	"appid" 	  => "wx39963eb0c927fc5e",
+    "mch_id"	  => "1487687822",//aapay
+    "sub_mch_id"  => "259088349",//qibee
+    "sub_appid"   => "wxfe7367b5ac549f65",
+    "sub_openid"  => "o8wR6092b7rNsRKO4cnwoz0QZF44",
+    "fee_type"    => "HKD",//usd
+    "key"		  => "jYPtbo3WGo6RoSeBY3vejN3pYTeQJY2R",
+    "secret"      => "7b9f13224885e04b6c3b1d3613b4bc98",
+    "redirect_uri"=> "http://qifanonline.com/wxpay/demo/wxpay.php",
+];
