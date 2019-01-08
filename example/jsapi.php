07c7624b1658506b04f56ff11d63320d3023b74a
Andy <tech-agent2@ipasspay.com>
2019-01-08 10:39:12 +0800

    -amss

--- example/jsapi.php
+++ example/jsapi.php
@@ -38,7 +38,7 @@ try{
 	$input->SetTime_start(date("YmdHis"));
 	$input->SetTime_expire(date("YmdHis", time() + 600));
 	$input->SetGoods_tag("test");
-	$input->SetNotify_url("http://qifanonline.com/wxpay/andy/notify.php");
+	$input->SetNotify_url("http://qifanonline.com/wxpay/demo/notifyUrl.php");
 	$input->SetTrade_type("JSAPI");
 	$input->SetOpenid($openId);
 	$config = new WxPayConfig();
