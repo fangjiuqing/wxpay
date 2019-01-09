f856eecd711794b5f239edb48b8cd308e3111443
Andy <tech-agent2@ipasspay.com>
2019-01-09 17:18:42 +0800

    aa

--- merchant.php
+++ merchant.php
@@ -13,7 +13,7 @@ $config['redirect_uri']    =    'http://qifanonline.com/wxpay/andy/merchant.php?
 $oop = new overSeaPay($config);
 $config['sub_openid'] = $oop->getOpenid();
 
-
+var_dump($config['redirect_uri']);die;
 ?>
 <html>
 <head>
